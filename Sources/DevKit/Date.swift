0d5442f0dbdbf8836700040d6c6855e359855dbb
Xiaofei GE <xfge.cn@gmail.com>
2024-04-16 21:13:12 +0800

    Add basics

---
+++ Sources/DevKit/Date.swift
@@ -0,0 +1,35 @@
+//
+//  Date.swift
+//
+//
+//  Created by Xiaofei Ge on 2024/4/16.
+//
+
+import Foundation
+
+extension Date {
+    func isEqual(to date: Date, toGranularity component: Calendar.Component, in calendar: Calendar = .current) -> Bool {
+        calendar.isDate(self, equalTo: date, toGranularity: component)
+    }
+
+    func isInSameYear(as date: Date) -> Bool { isEqual(to: date, toGranularity: .year) }
+    func isInSameMonth(as date: Date) -> Bool { isEqual(to: date, toGranularity: .month) }
+    func isInSameWeek(as date: Date) -> Bool { isEqual(to: date, toGranularity: .weekOfYear) }
+
+    func isInSameDay(as date: Date) -> Bool { Calendar.current.isDate(self, inSameDayAs: date) }
+
+    var isInThisYear:  Bool { isInSameYear(as: Date()) }
+    var isInThisMonth: Bool { isInSameMonth(as: Date()) }
+    var isInThisWeek:  Bool { isInSameWeek(as: Date()) }
+
+    var isInYesterday: Bool { Calendar.current.isDateInYesterday(self) }
+    var isInToday:     Bool { Calendar.current.isDateInToday(self) }
+    var isInTomorrow:  Bool { Calendar.current.isDateInTomorrow(self) }
+
+    var isInTheFuture: Bool { self > Date() }
+    var isInThePast:   Bool { self < Date() }
+    
+    var isValid:            Bool { self != .distantPast && self != .distantFuture }
+    var isDistantPast:      Bool { self == .distantPast }
+    var isDistantFuture:    Bool { self == .distantFuture }
+}
---
+++ Sources/DevKit/DateInterval.swift
@@ -0,0 +1,18 @@
+//
+//  DateInterval.swift
+//
+//
+//  Created by Xiaofei Ge on 2024/4/16.
+//
+
+import Foundation
+
+extension DateInterval {
+    init(safeStart start: Date, end: Date) {
+        if end < start {
+            self.init(start: start, end: start)
+        } else {
+            self.init(start: start, end: end)
+        }
+    }
+}
