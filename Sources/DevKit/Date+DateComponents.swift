0528839ca56f74df7d9e909a70a93de6e0bd0820
Xiaofei GE <xfge.cn@gmail.com>
2024-04-25 22:05:37 +0800

    Date+DateComponents

---
+++ Sources/DevKit/Date+DateComponents.swift
@@ -0,0 +1,33 @@
+//
+//  Date+DateComponents.swift
+//
+//
+//  Created by Xiaofei Ge on 2024/4/25.
+//
+
+import Foundation
+
+public extension Date {
+    init(month: Int, day: Int, year: Int, hour: Int = 0, minute: Int = 0, second: Int = 0) {
+        var dateComponents = DateComponents()
+        dateComponents.month = month
+        dateComponents.day = day
+        dateComponents.year = year
+        dateComponents.hour = hour
+        dateComponents.minute = minute
+        dateComponents.second = second
+        dateComponents.timeZone = .current
+        dateComponents.calendar = .current
+        self = Calendar.current.date(from: dateComponents) ?? Date()
+    }
+}
+
+public extension Date {
+    func get(_ components: Calendar.Component..., calendar: Calendar = Calendar.current) -> DateComponents {
+        return calendar.dateComponents(Set(components), from: self)
+    }
+
+    func get(_ component: Calendar.Component, calendar: Calendar = Calendar.current) -> Int {
+        return calendar.component(component, from: self)
+    }
+}
--- Sources/DevKit/Date.swift
+++ Sources/DevKit/Date.swift
@@ -295,18 +295,3 @@ public extension Date {
         return dateFormatter.string(from: self)
     }
 }
-
-extension Date {
-    init(month: Int, day: Int, year: Int, hour: Int = 0, minute: Int = 0, second: Int = 0) {
-        var dateComponents = DateComponents()
-        dateComponents.month = month
-        dateComponents.day = day
-        dateComponents.year = year
-        dateComponents.hour = hour
-        dateComponents.minute = minute
-        dateComponents.second = second
-        dateComponents.timeZone = .current
-        dateComponents.calendar = .current
-        self = Calendar.current.date(from: dateComponents) ?? Date()
-    }
-}
